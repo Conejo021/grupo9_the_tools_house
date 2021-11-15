888f9ed177eb7533684481e0c8fbae3ea110474b
andresurbano <andresx1000@hotmail.com>
2021-11-15 12:22:59 -0300

    actualizado

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const adminRouter = require('./routes/admin')
 const app = express();
 const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware');
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
